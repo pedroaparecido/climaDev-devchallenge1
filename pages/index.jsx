46fc28629d313bcf531d0d9c5ab5b5fd23ea4721
Pedro Ap <pedroaparecidori@gmail.com>
2024-05-15 16:46:01 -0300

    Melhorado index

--- pages/index.jsx
+++ pages/index.jsx
@@ -3,7 +3,12 @@ import styled from "styled-components"
 import BackgroundImageWithChildren from "../src/components/layout/backgroundImageWithChildren/BackgroundImageWithChildren"
 
 const StyledDiv = styled.div`
-  background-color: red;
+  background-color: ${props => props.theme.colors.white};
+  min-width: 500px;
+
+  @media (max-width: 500px) {
+    min-width: 100%;
+  }
 `
 
 export default function HomePage () {
