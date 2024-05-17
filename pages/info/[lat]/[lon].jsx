e2866b1bb4b0a6cd6bbeb95228781caeadf4a1b2
Pedro Ap <pedroaparecidori@gmail.com>
2024-05-17 14:20:40 -0300

    Criado container

--- pages/info/[lat]/[lon].jsx
+++ pages/info/[lat]/[lon].jsx
@@ -1,6 +1,7 @@
 import styled from "styled-components"
 
 import Navbar from "../../../src/components/navbar/Navbar"
+import Container from "../../../src/components/layout/container/Container"
 
 const Body = styled.div`
     background-color: ${props => props.theme.colors.grey};
@@ -11,7 +12,11 @@ export default function InfoPage() {
     return(
         <>
             <Navbar />
-            <Body>Conteudo</Body>
+            <Body>
+                <Container>
+                    Conteúdo
+                </Container>
+            </Body>
         </>
     )
 }
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,15 +1,20 @@
 import styled from "styled-components"
 
 import Logo from '../logo/Logo'
+import Container from "../layout/container/Container"
 
 const StyledNavbar = styled.div`
-
+    height: 105px;
+    display: flex;
+    align-items: center;
 `
 
 export default function Navbar() {
     return(
         <StyledNavbar>
-            <Logo />
+            <Container>
+                <Logo />
+            </Container>
         </StyledNavbar>
     )
 }
