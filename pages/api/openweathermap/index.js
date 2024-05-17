afec3dccb55c932527d130944e06ecd12791c90a
Pedro Ap <pedroaparecidori@gmail.com>
2024-05-17 17:09:57 -0300

    Criado decodeURI no handle

--- pages/api/openweathermap/index.js
+++ pages/api/openweathermap/index.js
@@ -1,5 +1,5 @@
 export default async function handle(req, res) {
-    const url = req.url.replace('/api/openweathermap?url=', '')
+    const url = decodeURIComponent(req.url).replace('/api/openweathermap?url=', '')
     const response = await fetch(`https://api.openweathermap.org/${url}&appid=${process.env.OWM_KEY}`)
     const json = await response.json()
     res.status(response.status).json(json)
