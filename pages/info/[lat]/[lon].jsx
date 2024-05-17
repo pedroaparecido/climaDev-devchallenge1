57a7eb1b9758d703918d25ed0ad4ed51ba7c3a0b
Pedro Ap <pedroaparecidori@gmail.com>
2024-05-17 15:14:10 -0300

    Criado climacard

--- pages/info/[lat]/[lon].jsx
+++ pages/info/[lat]/[lon].jsx
@@ -2,6 +2,7 @@ import styled from "styled-components"
 
 import Navbar from "../../../src/components/navbar/Navbar"
 import Container from "../../../src/components/layout/container/Container"
+import ClimaCard from "../../../src/components/climaCard/ClimaCard"
 
 const Body = styled.div`
     background-color: ${props => props.theme.colors.grey};
@@ -24,6 +25,13 @@ const CityName = styled.h2`
     margin: 0;
 `
 
+const InfoContainer = styled.div`
+    display: flex;
+    justify-content: space-between;
+    flex-wrap: wrap;
+    gap: 20px;
+`
+
 export default function InfoPage() {
     return(
         <>
@@ -34,6 +42,11 @@ export default function InfoPage() {
                         <Text>Previsão do tempo para</Text>
                         <CityName>Vitória, ES, Brasil</CityName>
                     </Content>
+                        <InfoContainer>
+                            <ClimaCard />
+                            <ClimaCard />
+                            <ClimaCard />
+                        </InfoContainer>
                 </Container>
             </Body>
         </>
---
+++ src/components/climaCard/ClimaCard.jsx
@@ -0,0 +1,76 @@
+import styled from "styled-components"
+
+const StyledClimaCard = styled.div`
+    background: ${props => props.theme.colors.white};
+    box-shadow: 0 4px 4px rgba(0, 0, 0, 0.25);
+    border-radius: 10px;
+    width: 350px;
+    padding: 15px;
+    box-sizing: border-box;
+`
+
+const Title = styled.h6`
+    font-weight: 700;
+    font-size: 20px;
+    text-align: center;
+    margin: 0;
+    padding: 0;
+`
+
+const BasicInfo = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+`
+
+const BasicInfoImg = styled.img`
+    width: 100px;
+    height: 100px;
+`
+
+const BasicInfoText = styled.div`
+    text-align: center;
+    flex: 1;
+`
+
+const InfoLine = styled.p`
+    display: flex;
+    justify-content: space-between;
+    font-size: 20px;
+    padding: 0 15px;
+`
+
+const InfoTemperature = styled.span`
+    font-weight: 700;
+
+    ${props => props.blue && 'color: #00C9F5;'}
+    ${props => props.red && 'color: #FF0000;'}
+`
+
+export default function ClimaCard() {
+    return(
+        <StyledClimaCard>
+            <Title>Titulo</Title>
+            <BasicInfo>
+                <BasicInfoImg src="http://openweathermap.org/img/wn/02d@2x.png" />
+                <BasicInfoText>Algumas nuvens</BasicInfoText>
+            </BasicInfo>
+                <InfoLine>
+                    Temperatura
+                    <InfoTemperature>29°C</InfoTemperature>
+                </InfoLine>
+                <InfoLine>
+                    Sensação
+                    <InfoTemperature>29°C</InfoTemperature>
+                </InfoLine>
+                <InfoLine>
+                    Mínima
+                    <InfoTemperature blue>29°C</InfoTemperature>
+                </InfoLine>
+                <InfoLine>
+                    Máxima
+                    <InfoTemperature red>29°C</InfoTemperature>
+                </InfoLine>
+        </StyledClimaCard>
+    )
+}
